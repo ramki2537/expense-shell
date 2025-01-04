51f8957d97698fc80357bc429afa6edca49b118e
Rama <ramki2537@gmail.com>
2025-01-05 00:18:44 +0800

    expense script

--- mysql.sh
+++ mysql.sh
@@ -35,7 +35,7 @@ echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
 
-dnf install mysql-server &>>$LOG_FILE_NAME
+dnf install mysql-server -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing mysql server"
 
 systemctl enable mysqld &>>$LOG_FILE_NAME
